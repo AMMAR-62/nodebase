4bf9e370ad7134189622918b82f88332fbc7837a
Ammar Shaikh <shaikhmohammedammar62@gmail.com>
2025-12-16 14:10:00 +0530

    02: db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import { Button } from '@/components/ui/button'
+import prisma from '@/lib/db'
 import { cn } from '@/lib/utils'
 
 import React from 'react'
 
-const Page = () => {
-
+const Page = async () => {
+  const users = await prisma.user.findMany()
   return (
     <div className={cn('min-h-screen min-w-screen flex items-center justify-center')}>
-      <Button variant={'outline'}>Click Me</Button>
+      <Button variant={'outline'}>{JSON.stringify(users)}</Button>
     </div>
   )
 }
